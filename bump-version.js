58db6751b37d1a000c31a29bebc5bb213f05a8e9
Cyril Strone <77802640+CyrilStrone@users.noreply.github.com>
2024-07-23 04:19:03 +0700

    Update.

--- bump-version.js
+++ bump-version.js
@@ -1,31 +1,13 @@
-import fs from 'fs'
-import path from 'path'
+import { readFileSync, writeFileSync } from 'fs'
+import { join } from 'path'
+import { inc } from 'semver'
 
-const packageJsonPath = path.resolve('package.json')
-const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'))
+const versionType = process.argv[2] || 'patch'
+const packageJsonPath = join(__dirname, 'package.json')
+const packageJson = JSON.parse(readFileSync(packageJsonPath, 'utf8'))
 
-const [, , bumpType = 'patch'] = process.argv
-const versionParts = packageJson.version.split('.').map(Number)
+packageJson.version = inc(packageJson.version, versionType)
 
-switch (bumpType) {
-  case 'major':
-    versionParts[0]++
-    versionParts[1] = 0
-    versionParts[2] = 0
-    break
-  case 'minor':
-    versionParts[1]++
-    versionParts[2] = 0
-    break
-  case 'patch':
-  default:
-    versionParts[2]++
-    break
-}
+writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2))
 
-const newVersion = versionParts.join('.')
-packageJson.version = newVersion
-
-fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2) + '\n')
-
-console.log(`Version bumped to ${newVersion}`)
+console.log(`Version bumped to ${packageJson.version}`)
