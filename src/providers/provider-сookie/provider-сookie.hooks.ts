c775819248657d9c57b5d8b75a5f1d7f092d9c38
Cyril Strone <77802640+CyrilStrone@users.noreply.github.com>
2024-07-29 05:40:05 +0700

    Update.

--- src/providers/provider-сookie/provider-сookie.hooks.ts
+++ src/providers/provider-сookie/provider-сookie.hooks.ts
@@ -1,6 +1,6 @@
-import { useContext } from 'react'
+import { useCallback, useContext, useState } from 'react'
 
-import { CookieContext } from '.'
+import { CookieContext, ValidCookieObject } from '.'
 
 export const useCookie = () => {
   const context = useContext(CookieContext)
@@ -8,4 +8,33 @@ export const useCookie = () => {
     throw new Error('useCookie must be used within an ProviderCookie')
   }
   return context
+}
+
+export const useCookieKey = (key: keyof ValidCookieObject) => {
+  const { getCookie, removeCookie, setCookie } = useCookie()
+
+  const [value, setValue] = useState<ValidCookieObject[typeof key] | undefined>(
+    getCookie(key),
+  )
+
+  const updateValue = useCallback(
+    (
+      newValue: ValidCookieObject[typeof key],
+      options?: Cookies.CookieAttributes,
+    ) => {
+      setCookie(key, newValue, options)
+      setValue(newValue)
+    },
+    [key, setCookie],
+  )
+
+  const deleteValue = useCallback(
+    (options?: Cookies.CookieAttributes) => {
+      removeCookie(key, options)
+      setValue(undefined)
+    },
+    [key, removeCookie],
+  )
+
+  return [value, updateValue, deleteValue] as const
 }
