770973d19b5acbdc9bc67d31a6ca59ce149ae14a
Cyril Strone <77802640+CyrilStrone@users.noreply.github.com>
2024-07-31 22:21:20 +0700

    Update.

--- src/providers/provider-сookie/provider-сookie.tsx
+++ src/providers/provider-сookie/provider-сookie.tsx
@@ -1,60 +1,82 @@
 import Cookies from 'js-cookie'
 import { FC, createContext, useCallback, useEffect, useState } from 'react'
 
-import { CookieContextProps, ProviderCookieProps, ValidCookieObject } from '.'
+import {
+  CookieAttributes,
+  CookieContextProps,
+  ProviderCookieProps,
+  ValidCookieObject,
+} from '.'
 
 export const CookieContext = createContext<CookieContextProps | null>(null)
 
-export const ProviderCookie: FC<ProviderCookieProps> = ({ children }) => {
+export const ProviderCookie: FC<ProviderCookieProps> = ({
+  children,
+  validKeys,
+}) => {
   const [cookie, setCookie] = useState<ValidCookieObject>()
 
-  const getCookie: CookieContextProps['getCookie'] = useCallback((name) => {
-    const cookie = Cookies.get(String(name))
-    return cookie ? JSON.parse(cookie) : undefined
-  }, [])
 
-  // const isValidCookieValue = useCallback(
-  //   <K extends keyof ValidCookieObject>(
-  //     key: K,
-  //     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  //     value: any,
-  //   ): value is ValidCookieObject[K] => {
-  //     const expectedType = typeof ({} as ValidCookieObject)[key]
-  //     switch (expectedType) {
-  //       case 'string':
-  //         return typeof value === 'string'
-  //       case 'number':
-  //         return typeof value === 'number'
-  //       case 'boolean':
-  //         return typeof value === 'boolean'
-  //       case 'object':
-  //         if (value === null || Array.isArray(value)) return false
-  //         return Object.entries(value).every(([subKey, subValue]) => {
-  //           if (typeof subKey !== 'string') return false
-  //           return isValidCookieValue(subKey, subValue)
-  //         })
-  //       default:
-  //         return false
-  //     }
-  //   },
-  //   [],
-  // )
+  const isValidCookieValue = useCallback(
+    <K extends keyof ValidCookieObject>(
+      key: K,
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      value: any,
+    ): value is ValidCookieObject[K] => {
+      const checkValue = value as (ValidCookieObject)[typeof key]
+      switch (typeof checkValue) {
+        case 'string':
+          return typeof value === 'string'
+        case 'number':
+          return typeof value === 'number'
+        case 'boolean':
+          return typeof value === 'boolean'
+        case 'object':
+          if (value === null || Array.isArray(value)) return false
+          return Object.entries(value).every(([subKey, subValue]) => {
+            return (
+              typeof subKey === 'string' &&
+              isValidCookieValue(subKey as keyof ValidCookieObject, subValue)
+            )
+          })
+        default:
+          return false
+      }
+    },
+    [],
+  )
 
-  const changeCookie: CookieContextProps['setCookie'] = (
-    name,
-    value,
-    options,
-  ) => {
-    try {
-      Cookies.set(String(name), JSON.stringify(value), options)
-      setCookie((prevState) => ({ ...prevState, [name]: value }))
-    } catch {
-      console.error(`Error. setCookie - key:${name}, value:${value}`)
-    }
-  }
+  const getCookie = useCallback(
+    <K extends keyof ValidCookieObject>(
+      name: K,
+    ): ValidCookieObject[K] | undefined => {
+      const cookie = Cookies.get(String(name))
+      return cookie ? JSON.parse(cookie) : undefined
+    },
+    [],
+  )
 
-  const removeCookie: CookieContextProps['removeCookie'] = useCallback(
-    (name, options) => {
+  const changeCookie = useCallback(
+    <K extends keyof ValidCookieObject>(
+      name: K,
+      value: ValidCookieObject[K],
+      options?: CookieAttributes,
+    ) => {
+      try {
+        Cookies.set(String(name), JSON.stringify(value), options)
+        setCookie((prevState) => ({ ...prevState, [name]: value }))
+      } catch {
+        console.error(`Error. setCookie - key:${name}, value:${value}`)
+      }
+    },
+    [],
+  )
+
+  const removeCookie = useCallback(
+    <K extends keyof ValidCookieObject>(
+      name: K,
+      options?: CookieAttributes,
+    ) => {
       try {
         Cookies.remove(String(name), options)
         setCookie((prevState) => ({ ...prevState, [name]: undefined }))
@@ -65,23 +87,29 @@ export const ProviderCookie: FC<ProviderCookieProps> = ({ children }) => {
     [],
   )
 
-  useEffect(() => {
-    const validKeys = Object.keys({}) as (keyof ValidCookieObject)[]
-
-    validKeys.forEach((key) => {
+  const checkCookie = useCallback(() => {
+    (validKeys ?? []).forEach((key) => {
       const cookieValue = Cookies.get(String(key))
       if (cookieValue) {
         try {
           const parsedValue = JSON.parse(cookieValue)
-          setCookie((prevState) => ({ ...prevState, [key]: parsedValue }))
+          if (!isValidCookieValue(key, parsedValue)) {
+            removeCookie(String(key) as never)
+          } else {
+            setCookie((prevState) => ({ ...prevState, [key]: parsedValue }))
+          }
         } catch {
-          if (String(key)) removeCookie(String(key))
+          removeCookie(String(key) as never)
         }
       } else {
-        removeCookie(String(key))
+        removeCookie(String(key) as never)
       }
     })
-  }, [removeCookie])
+  }, [isValidCookieValue, removeCookie, validKeys])
+
+  useEffect(() => {
+    checkCookie()
+  }, [checkCookie])
 
   return (
     <CookieContext.Provider
@@ -89,6 +117,7 @@ export const ProviderCookie: FC<ProviderCookieProps> = ({ children }) => {
         getCookie,
         setCookie: changeCookie,
         removeCookie,
+        checkCookie,
         cookie,
       }}
     >
