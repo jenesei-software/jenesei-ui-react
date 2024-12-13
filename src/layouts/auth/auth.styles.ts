720d42cbbe03a233677a85ef44bac156beb9264b
Cyril Strone <77802640+CyrilStrone@users.noreply.github.com>
2024-12-14 06:01:23 +0700

    Feat

--- src/layouts/auth/auth.styles.ts
+++ src/layouts/auth/auth.styles.ts
@@ -1,53 +1,49 @@
+import { motion } from 'framer-motion'
 import Image from 'react-image-webp'
 import styled from 'styled-components'
 
-export const AuthLayoutWrapper = styled.div`
-  display: flex;
+export const AuthLayoutWrapper = styled(motion.div)`
+  display: grid;
   width: 100%;
   height: 100dvh;
-  overflow: hidden;
-  flex-direction: row;
-  @media (max-width: ${props => props.theme.screens.tablet.width}) {
-    flex-direction: column;
-  }
+  position: relative;
 `
 export const AuthStyledImage = styled(Image)`
   width: 100%;
   height: 100%;
   position: absolute;
   object-fit: cover;
-  object-position: center;
+  object-position: left;
   left: 0;
   top: 0;
   background-color: ${props => props.theme.palette.black100};
-  z-index: 0;
-`
-export const AuthLayoutWrapperBackground = styled.div`
-  position: relative;
-  flex-grow: 1;
-  width: 50%;
-  height: 100%;
-  display: flex;
-  align-items: stretch;
-  flex-direction: column;
-  justify-content: space-between;
-  padding: 100px 100px;
   @media (max-width: ${props => props.theme.screens.tablet.width}) {
-    height: 170px;
+    height: 100%;
     width: 100%;
-    justify-content: center;
-    padding: 0px 100px;
-  }
-  @media (max-width: ${props => props.theme.screens.mobile.width}) {
-    align-items: center;
-    justify-content: center;
+    object-position: bottom;
   }
 `
 
-export const AuthLayoutWrapperChildren = styled.div`
-  flex-grow: 1;
-  width: 50%;
-  height: 100%;
+export const AuthLayoutWrapperBackground = styled(motion.div)`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  padding: 100px 100px;
+  @media (max-width: ${props => props.theme.screens.tablet.width}) {
+    padding: 0px 100px;
+  }
+  @media (max-width: ${props => props.theme.screens.mobile.width}) {
+  }
+`
+
+export const AuthLayoutWrapperChildren = styled.svg`
+  background-color: ${props => props.theme.palette.whiteStandard};
+  z-index: 0;
+`
+
+export const AuthLayoutWrapperChildrenWrapper = styled(motion.div)`
+  background-color: ${props => props.theme.palette.whiteStandard};
+  z-index: 0;
   padding: 80px;
   display: flex;
   align-items: center;
@@ -57,7 +53,6 @@ export const AuthLayoutWrapperChildren = styled.div`
     justify-content: center;
     align-items: flex-start;
     width: 100%;
-    height: calc(100% - 170px);
     padding: 100px;
   }
   @media (max-width: ${props => props.theme.screens.mobile.width}) {
@@ -67,11 +62,11 @@ export const AuthLayoutWrapperChildren = styled.div`
   }
 `
 
-export const AuthLayoutWrapperBackgroundSVG = styled.svg`
+export const AuthLayoutWrapperBackgroundSVG = styled(motion.svg)`
   z-index: 1;
 `
 
-export const AuthLayoutWrapperBackgroundText = styled.div`
+export const AuthLayoutWrapperBackgroundText = styled(motion.div)`
   z-index: 1;
   display: flex;
   flex-direction: column;
