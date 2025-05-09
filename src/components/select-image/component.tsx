a13ef554e70e94f3fbe796c5a3d3b0c270c8ac1d
Cyril Strone <77802640+CyrilStrone@users.noreply.github.com>
2025-05-10 04:45:16 +0700

    feat

--- src/components/select-image/component.tsx
+++ src/components/select-image/component.tsx
@@ -3,6 +3,7 @@ import { DragEvent, useCallback, useEffect, useMemo, useRef, useState } from 're
 import { useTheme } from 'styled-components'
 
 import { useDialog } from '@local/contexts/context-dialog'
+import { ErrorMessage } from '@local/styles/error'
 import { KEY_SIZE_DATA } from '@local/theme'
 
 import { SelectImageItemProps, SelectImageListWrapper, SelectImageProps, SelectImageWrapper } from '.'
@@ -159,194 +160,203 @@ export const SelectImage = (props: SelectImageProps) => {
     [add, br, props.locale.textFallbackImage]
   )
   return (
-    <SelectImageWrapper
-      $genre={props.genre}
-      $size={props.size}
-      id={props.id}
-      $width={props.width}
-      $isError={props.isError}
-    >
-      <SelectImageListWrapper
-        onDrop={e => {
-          handleDrop(e)
-          setIsDraggingOver(false)
-        }}
-        onDragOver={e => e.preventDefault()}
-        onDragEnter={() => setIsDraggingOver(true)}
-        onDragLeave={() => setIsDraggingOver(false)}
-        animate={{
-          borderColor: isDraggingOver
-            ? theme.colors.selectImage[props.genre].border.hover
-            : theme.colors.selectImage[props.genre].border.rest
-        }}
-        transition={{ duration: 0.3 }}
+    <>
+      <SelectImageWrapper
         $genre={props.genre}
         $size={props.size}
+        id={props.id}
+        $width={props.width}
+        $isError={props.isError}
       >
-        <motion.div
-          transition={{
-            layout: {
-              duration: 0.3,
-              ease: 'easeInOut',
-              type: 'spring'
-            }
+        <SelectImageListWrapper
+          onDrop={e => {
+            handleDrop(e)
+            setIsDraggingOver(false)
           }}
-          layout
-          style={{ display: 'flex', flexWrap: 'wrap', gap: `${size.padding - 2}px` }}
+          onDragOver={e => e.preventDefault()}
+          onDragEnter={() => setIsDraggingOver(true)}
+          onDragLeave={() => setIsDraggingOver(false)}
+          animate={{
+            borderColor: isDraggingOver
+              ? theme.colors.selectImage[props.genre].border.hover
+              : theme.colors.selectImage[props.genre].border.rest
+          }}
+          transition={{ duration: 0.3 }}
+          $genre={props.genre}
+          $size={props.size}
         >
-          {images.map(
-            img =>
-              img.url && (
-                <motion.div
-                  key={img.id}
-                  style={{
-                    position: 'relative',
-                    width: `${props.imageSettings.width}px`,
-                    height: `${props.imageSettings.height}px`,
-                    userSelect: 'none',
-                    overflow: 'hidden',
-                    borderRadius: `${size.radius}px`,
-                    flexGrow: 1
-                  }}
-                  layout
-                  transition={{
-                    layout: {
-                      duration: 0.3,
-                      ease: 'easeInOut',
-                      type: 'spring'
-                    }
-                  }}
-                >
-                  <Image
-                    propsStack={{
-                      sx: theme => ({
+          <motion.div
+            transition={{
+              layout: {
+                duration: 0.3,
+                ease: 'easeInOut',
+                type: 'spring'
+              }
+            }}
+            layout
+            style={{ display: 'flex', flexWrap: 'wrap', gap: `${size.padding - 2}px` }}
+          >
+            {images.map(
+              img =>
+                img.url && (
+                  <motion.div
+                    key={img.id}
+                    style={{
+                      position: 'relative',
+                      width: `${props.imageSettings.width}px`,
+                      height: `${props.imageSettings.height}px`,
+                      userSelect: 'none',
+                      overflow: 'hidden',
+                      borderRadius: `${size.radius}px`,
+                      flexGrow: 1
+                    }}
+                    layout
+                    transition={{
+                      layout: {
+                        duration: 0.3,
+                        ease: 'easeInOut',
+                        type: 'spring'
+                      }
+                    }}
+                  >
+                    <Image
+                      propsStack={{
+                        sx: theme => ({
+                          default: {
+                            width: '100%',
+                            height: '100%',
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                            backgroundColor: theme.palette.black10,
+                            position: 'absolute',
+                            pointerEvents: 'none'
+                          }
+                        })
+                      }}
+                      alt={img.name || 'image'}
+                      src={img.url}
+                      fallback={<Typography variant="h6">{props.locale.textFallbackImage}</Typography>}
+                    />
+                    <Button
+                      sx={{
                         default: {
-                          width: '100%',
-                          height: '100%',
-                          alignItems: 'center',
-                          justifyContent: 'center',
-                          backgroundColor: theme.palette.black10,
                           position: 'absolute',
-                          pointerEvents: 'none'
+                          top: 5,
+                          right: 5
                         }
-                      })
-                    }}
-                    alt={img.name || 'image'}
-                    src={img.url}
-                    fallback={<Typography variant="h6">{props.locale.textFallbackImage}</Typography>}
-                  />
-                  <Button
-                    sx={{
-                      default: {
-                        position: 'absolute',
-                        top: 5,
-                        right: 5
-                      }
-                    }}
-                    genre={props.genre}
-                    size="small"
-                    icon={{
-                      type: 'id',
-                      name: 'Close'
-                    }}
-                    width="asHeight"
-                    isHiddenBorder
-                    isRadius
-                    onClick={() => handleDelete(img.id)}
-                  />
-                  <Button
-                    sx={{
-                      default: {
-                        position: 'absolute',
-                        bottom: 5,
-                        right: 5
-                      }
-                    }}
-                    genre={props.genre}
-                    size="small"
-                    icon={{
-                      type: 'id',
-                      name: 'Activity'
-                    }}
-                    width="asHeight"
-                    isHiddenBorder
-                    isRadius
-                    onClick={() => img.url && handleAdd({ id: img.id, imageSrc: img.url })}
-                  />
-                </motion.div>
-              )
-          )}
-          {images.length === 0 && (
-            <div
-              onClick={openFileDialog}
-              key="empty"
-              style={{
-                position: 'relative',
-                width: `${props.imageSettings.width}px`,
-                height: `${props.imageSettings.height}px`,
-                userSelect: 'none',
-                overflow: 'hidden',
-                borderRadius: `${size.radius}px`,
-                flexGrow: 1,
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'center',
-                textAlign: 'center',
-                cursor: 'pointer'
-              }}
-            >
-              <Typography
+                      }}
+                      genre={props.genre}
+                      size="small"
+                      icon={{
+                        type: 'id',
+                        name: 'Close'
+                      }}
+                      width="asHeight"
+                      isHiddenBorder
+                      isRadius
+                      onClick={() => handleDelete(img.id)}
+                    />
+                    <Button
+                      sx={{
+                        default: {
+                          position: 'absolute',
+                          bottom: 5,
+                          right: 5
+                        }
+                      }}
+                      genre={props.genre}
+                      size="small"
+                      icon={{
+                        type: 'id',
+                        name: 'Activity'
+                      }}
+                      width="asHeight"
+                      isHiddenBorder
+                      isRadius
+                      onClick={() => img.url && handleAdd({ id: img.id, imageSrc: img.url })}
+                    />
+                  </motion.div>
+                )
+            )}
+            {images.length === 0 && (
+              <div
+                onClick={openFileDialog}
+                key="empty"
                 style={{
-                  color: theme.colors.selectImage[props.genre].color.rest
+                  position: 'relative',
+                  width: `${props.imageSettings.width}px`,
+                  height: `${props.imageSettings.height}px`,
+                  userSelect: 'none',
+                  overflow: 'hidden',
+                  borderRadius: `${size.radius}px`,
+                  flexGrow: 1,
+                  display: 'flex',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  textAlign: 'center',
+                  cursor: 'pointer'
                 }}
               >
-                {props.locale.dragAndDrop}
-              </Typography>
-            </div>
-          )}
-        </motion.div>
+                <Typography
+                  style={{
+                    color: theme.colors.selectImage[props.genre].color.rest
+                  }}
+                >
+                  {props.locale.dragAndDrop}
+                </Typography>
+              </div>
+            )}
+          </motion.div>
 
-        <input
-          ref={inputRef}
-          type="file"
-          accept="image/*"
-          multiple
-          style={{ display: 'none' }}
-          onChange={e => {
-            if (e.target.files) handleAddFiles(e.target.files)
-          }}
-        />
-      </SelectImageListWrapper>
-      <Stack sx={{ default: { flexGrow: 1, gap: `${size.padding - 2}px` } }}>
-        <Button
-          type="button"
-          genre={props.genre}
-          size={props.size}
-          sx={{
-            default: {
-              flexGrow: 3
-            }
-          }}
-          isRadius
-          onClick={openFileDialog}
-        >
-          {props.locale.textAddImage}
-        </Button>
-        <Button
-          isRadius
-          type="button"
-          sx={{
-            default: {
-              flexGrow: 1
-            }
-          }}
-          onClick={resetImages}
-          genre={props.genre}
-          size={props.size}
-        >
-          {props.locale.textResetImage}
-        </Button>
-      </Stack>
-    </SelectImageWrapper>
+          <input
+            ref={inputRef}
+            type="file"
+            accept="image/*"
+            multiple
+            style={{ display: 'none' }}
+            onChange={e => {
+              if (e.target.files) handleAddFiles(e.target.files)
+            }}
+          />
+        </SelectImageListWrapper>
+        <Stack sx={{ default: { flexGrow: 1, gap: `${size.padding - 2}px` } }}>
+          <Button
+            type="button"
+            genre={props.genre}
+            size={props.size}
+            sx={{
+              default: {
+                flexGrow: 3
+              }
+            }}
+            isRadius
+            onClick={openFileDialog}
+          >
+            {props.locale.textAddImage}
+          </Button>
+          <Button
+            isRadius
+            type="button"
+            sx={{
+              default: {
+                flexGrow: 1
+              }
+            }}
+            onClick={resetImages}
+            genre={props.genre}
+            size={props.size}
+          >
+            {props.locale.textResetImage}
+          </Button>
+        </Stack>
+      </SelectImageWrapper>
+      <ErrorMessage
+        isError={props.isError}
+        errorMessage={props.errorMessage}
+        size={props.size}
+        width={props.width}
+        isErrorAbsolute={props.isErrorAbsolute}
+      />
+    </>
   )
 }
